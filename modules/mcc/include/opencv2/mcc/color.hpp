4e1adae15ef32bff79de3ecfbb3fe6dafc922d74
JinhengZhang <jinheng_zhang@foxmail.com>
2020-08-29 10:00:34 +0800

    fix again

--- modules/mcc/include/opencv2/mcc/color.hpp
+++ modules/mcc/include/opencv2/mcc/color.hpp
@@ -40,173 +40,173 @@ namespace ccm
 {
 
 /* color defined by color_values and color space;
-	grays is mask of grayscale color;
-	colored is mask of colored color;
-	history is storage of historical conversion; */
+    grays is mask of grayscale color;
+    colored is mask of colored color;
+    history is storage of historical conversion; */
 class Color 
 {
 public:
-	cv::Mat colors;
-	const ColorSpace& cs;
-	cv::Mat grays;
-	cv::Mat colored;
-	std::map<ColorSpace, std::shared_ptr<Color>> history;
+    cv::Mat colors;
+    const ColorSpace& cs;
+    cv::Mat grays;
+    cv::Mat colored;
+    std::map<ColorSpace, std::shared_ptr<Color>> history;
 
-	Color(cv::Mat colors, const ColorSpace& cs) :colors(colors), cs(cs) {};
+    Color(cv::Mat colors, const ColorSpace& cs) :colors(colors), cs(cs) {};
 
-	virtual ~Color() {};
+    virtual ~Color() {};
 
-	/* change to other color space; return Color;
-		The conversion process incorporates linear transformations to speed up.
-		method is chromatic adapation method;
-		when save if True, get data from history first; */
-	Color to(const ColorSpace& other, CAM method = BRADFORD, bool save = true) 
-	{
-		if (history.count(other) == 1) 
-		{
+    /* change to other color space; return Color;
+        The conversion process incorporates linear transformations to speed up.
+        method is chromatic adapation method;
+        when save if True, get data from history first; */
+    Color to(const ColorSpace& other, CAM method = BRADFORD, bool save = true) 
+    {
+        if (history.count(other) == 1) 
+        {
 
-			return *history[other];
-		}
-		if (cs.relate(other)) 
-		{
-			return Color(cs.relation(other).run(colors), other);
-		}
-		Operations ops;
-		ops.add(cs.to).add(XYZ(cs.io).cam(other.io, method)).add(other.from);
-		std::shared_ptr<Color> color(new Color(ops.run(colors), other));
-		if (save) 
-		{
-			history[other] = color;
-		}
-		return *color;
-	}
+            return *history[other];
+        }
+        if (cs.relate(other)) 
+        {
+            return Color(cs.relation(other).run(colors), other);
+        }
+        Operations ops;
+        ops.add(cs.to).add(XYZ(cs.io).cam(other.io, method)).add(other.from);
+        std::shared_ptr<Color> color(new Color(ops.run(colors), other));
+        if (save) 
+        {
+            history[other] = color;
+        }
+        return *color;
+    }
 
-	cv::Mat channel(cv::Mat m, int i) 
-	{
-		cv::Mat dchannels[3];
-		split(m, dchannels);
-		return dchannels[i];
-	}
+    cv::Mat channel(cv::Mat m, int i) 
+    {
+        cv::Mat dchannels[3];
+        split(m, dchannels);
+        return dchannels[i];
+    }
 
-	cv::Mat toGray(IO io, CAM method = BRADFORD, bool save = true) 
-	{
-		XYZ xyz(io); 
-		return channel(this->to(xyz, method, save).colors, 1);
-	}
+    cv::Mat toGray(IO io, CAM method = BRADFORD, bool save = true) 
+    {
+        XYZ xyz(io); 
+        return channel(this->to(xyz, method, save).colors, 1);
+    }
 
-	cv::Mat toLuminant(IO io, CAM method = BRADFORD, bool save = true) 
-	{
-		Lab lab(io);
-		return channel(this->to(lab, method, save).colors, 0);
-	}
+    cv::Mat toLuminant(IO io, CAM method = BRADFORD, bool save = true) 
+    {
+        Lab lab(io);
+        return channel(this->to(lab, method, save).colors, 0);
+    }
 
-	/* return distance between self and other */
-	cv::Mat diff(Color& other, DISTANCE_TYPE method = CIE2000) 
-	{
-		return diff(other, cs.io, method);
-	}
+    /* return distance between self and other */
+    cv::Mat diff(Color& other, DISTANCE_TYPE method = CIE2000) 
+    {
+        return diff(other, cs.io, method);
+    }
 
-	/**\return distance between this and other */
-	cv::Mat diff(Color& other, IO io, DISTANCE_TYPE method = CIE2000) 
-	{
-		Lab lab(io);
-		switch (method)
-		{
-		case cv::ccm::CIE76:
-		case cv::ccm::CIE94_GRAPHIC_ARTS:
-		case cv::ccm::CIE94_TEXTILES:
-		case cv::ccm::CIE2000:
-		case cv::ccm::CMC_1TO1:
-		case cv::ccm::CMC_2TO1:
-			return distance(to(lab).colors, other.to(lab).colors, method);
-		case cv::ccm::RGB:
-			return distance(to(*cs.nl).colors, other.to(*cs.nl).colors, method);
-		case cv::ccm::RGBL:
-			return distance(to(*cs.l).colors, other.to(*cs.l).colors, method);
-		default:
-			throw std::invalid_argument { "Wrong method!" };
-			break;
-		}
-	}
+    /**\return distance between this and other */
+    cv::Mat diff(Color& other, IO io, DISTANCE_TYPE method = CIE2000) 
+    {
+        Lab lab(io);
+        switch (method)
+        {
+        case cv::ccm::CIE76:
+        case cv::ccm::CIE94_GRAPHIC_ARTS:
+        case cv::ccm::CIE94_TEXTILES:
+        case cv::ccm::CIE2000:
+        case cv::ccm::CMC_1TO1:
+        case cv::ccm::CMC_2TO1:
+            return distance(to(lab).colors, other.to(lab).colors, method);
+        case cv::ccm::RGB:
+            return distance(to(*cs.nl).colors, other.to(*cs.nl).colors, method);
+        case cv::ccm::RGBL:
+            return distance(to(*cs.l).colors, other.to(*cs.l).colors, method);
+        default:
+            throw std::invalid_argument { "Wrong method!" };
+            break;
+        }
+    }
 
-	/* calculate gray mask */
-	void getGray(double JDN = 2.0) 
-	{
-		cv::Mat lab = to(Lab_D65_2).colors;
-		cv::Mat gray(colors.size(), colors.type());
-		int fromto[] = { 0,0, -1,1, -1,2 };
-		mixChannels(&lab, 1, &gray, 1, fromto, 3);
-		cv::Mat d = distance(lab, gray, CIE2000);
-		this->grays = d < JDN;
-		this->colored = ~grays;
-	}
+    /* calculate gray mask */
+    void getGray(double JDN = 2.0) 
+    {
+        cv::Mat lab = to(Lab_D65_2).colors;
+        cv::Mat gray(colors.size(), colors.type());
+        int fromto[] = { 0,0, -1,1, -1,2 };
+        mixChannels(&lab, 1, &gray, 1, fromto, 3);
+        cv::Mat d = distance(lab, gray, CIE2000);
+        this->grays = d < JDN;
+        this->colored = ~grays;
+    }
 
-	Color operator[](cv::Mat mask) 
-	{
-		return Color(maskCopyTo(colors, mask), cs);
-	}
+    Color operator[](cv::Mat mask) 
+    {
+        return Color(maskCopyTo(colors, mask), cs);
+    }
 
-	Color operator=(Color inp) 
-	{
-		return inp;
-	}
+    Color operator=(Color inp) 
+    {
+        return inp;
+    }
 };
 
 
 /* Data is from https://www.imatest.com/wp-content/uploads/2011/11/Lab-data-Iluminate-D65-D50-spectro.xls
-	see Miscellaneous.md for details. */
+    see Miscellaneous.md for details. */
 
 const cv::Mat ColorChecker2005_LAB_D50_2 = (cv::Mat_<cv::Vec3d>(24, 1) <<
-	cv::Vec3d(37.986, 13.555, 14.059),
-	cv::Vec3d(65.711, 18.13, 17.81),
-	cv::Vec3d(49.927, -4.88, -21.925),
-	cv::Vec3d(43.139, -13.095, 21.905),
-	cv::Vec3d(55.112, 8.844, -25.399),
-	cv::Vec3d(70.719, -33.397, -0.199),
-	cv::Vec3d(62.661, 36.067, 57.096),
-	cv::Vec3d(40.02, 10.41, -45.964),
-	cv::Vec3d(51.124, 48.239, 16.248),
-	cv::Vec3d(30.325, 22.976, -21.587),
-	cv::Vec3d(72.532, -23.709, 57.255),
-	cv::Vec3d(71.941, 19.363, 67.857),
-	cv::Vec3d(28.778, 14.179, -50.297),
-	cv::Vec3d(55.261, -38.342, 31.37),
-	cv::Vec3d(42.101, 53.378, 28.19),
-	cv::Vec3d(81.733, 4.039, 79.819),
-	cv::Vec3d(51.935, 49.986, -14.574),
-	cv::Vec3d(51.038, -28.631, -28.638),
-	cv::Vec3d(96.539, -0.425, 1.186),
-	cv::Vec3d(81.257, -0.638, -0.335),
-	cv::Vec3d(66.766, -0.734, -0.504),
-	cv::Vec3d(50.867, -0.153, -0.27),
-	cv::Vec3d(35.656, -0.421, -1.231),
-	cv::Vec3d(20.461, -0.079, -0.973));
+    cv::Vec3d(37.986, 13.555, 14.059),
+    cv::Vec3d(65.711, 18.13, 17.81),
+    cv::Vec3d(49.927, -4.88, -21.925),
+    cv::Vec3d(43.139, -13.095, 21.905),
+    cv::Vec3d(55.112, 8.844, -25.399),
+    cv::Vec3d(70.719, -33.397, -0.199),
+    cv::Vec3d(62.661, 36.067, 57.096),
+    cv::Vec3d(40.02, 10.41, -45.964),
+    cv::Vec3d(51.124, 48.239, 16.248),
+    cv::Vec3d(30.325, 22.976, -21.587),
+    cv::Vec3d(72.532, -23.709, 57.255),
+    cv::Vec3d(71.941, 19.363, 67.857),
+    cv::Vec3d(28.778, 14.179, -50.297),
+    cv::Vec3d(55.261, -38.342, 31.37),
+    cv::Vec3d(42.101, 53.378, 28.19),
+    cv::Vec3d(81.733, 4.039, 79.819),
+    cv::Vec3d(51.935, 49.986, -14.574),
+    cv::Vec3d(51.038, -28.631, -28.638),
+    cv::Vec3d(96.539, -0.425, 1.186),
+    cv::Vec3d(81.257, -0.638, -0.335),
+    cv::Vec3d(66.766, -0.734, -0.504),
+    cv::Vec3d(50.867, -0.153, -0.27),
+    cv::Vec3d(35.656, -0.421, -1.231),
+    cv::Vec3d(20.461, -0.079, -0.973));
 
 const cv::Mat ColorChecker2005_LAB_D65_2 = (cv::Mat_<cv::Vec3d>(24, 1) <<
-	cv::Vec3d(37.542, 12.018, 13.33),
-	cv::Vec3d(65.2, 14.821, 17.545),
-	cv::Vec3d(50.366, -1.573, -21.431),
-	cv::Vec3d(43.125, -14.63, 22.12),
-	cv::Vec3d(55.343, 11.449, -25.289),
-	cv::Vec3d(71.36, -32.718, 1.636),
-	cv::Vec3d(61.365, 32.885, 55.155),
-	cv::Vec3d(40.712, 16.908, -45.085),
-	cv::Vec3d(49.86, 45.934, 13.876),
-	cv::Vec3d(30.15, 24.915, -22.606),
-	cv::Vec3d(72.438, -27.464, 58.469),
-	cv::Vec3d(70.916, 15.583, 66.543),
-	cv::Vec3d(29.624, 21.425, -49.031),
-	cv::Vec3d(55.643, -40.76, 33.274),
-	cv::Vec3d(40.554, 49.972, 25.46),
-	cv::Vec3d(80.982, -1.037, 80.03),
-	cv::Vec3d(51.006, 49.876, -16.93),
-	cv::Vec3d(52.121, -24.61, -26.176),
-	cv::Vec3d(96.536, -0.694, 1.354),
-	cv::Vec3d(81.274, -0.61, -0.24),
-	cv::Vec3d(66.787, -0.647, -0.429),
-	cv::Vec3d(50.872, -0.059, -0.247),
-	cv::Vec3d(35.68, -0.22, -1.205),
-	cv::Vec3d(20.475, 0.049, -0.972));
+    cv::Vec3d(37.542, 12.018, 13.33),
+    cv::Vec3d(65.2, 14.821, 17.545),
+    cv::Vec3d(50.366, -1.573, -21.431),
+    cv::Vec3d(43.125, -14.63, 22.12),
+    cv::Vec3d(55.343, 11.449, -25.289),
+    cv::Vec3d(71.36, -32.718, 1.636),
+    cv::Vec3d(61.365, 32.885, 55.155),
+    cv::Vec3d(40.712, 16.908, -45.085),
+    cv::Vec3d(49.86, 45.934, 13.876),
+    cv::Vec3d(30.15, 24.915, -22.606),
+    cv::Vec3d(72.438, -27.464, 58.469),
+    cv::Vec3d(70.916, 15.583, 66.543),
+    cv::Vec3d(29.624, 21.425, -49.031),
+    cv::Vec3d(55.643, -40.76, 33.274),
+    cv::Vec3d(40.554, 49.972, 25.46),
+    cv::Vec3d(80.982, -1.037, 80.03),
+    cv::Vec3d(51.006, 49.876, -16.93),
+    cv::Vec3d(52.121, -24.61, -26.176),
+    cv::Vec3d(96.536, -0.694, 1.354),
+    cv::Vec3d(81.274, -0.61, -0.24),
+    cv::Vec3d(66.787, -0.647, -0.429),
+    cv::Vec3d(50.872, -0.059, -0.247),
+    cv::Vec3d(35.68, -0.22, -1.205),
+    cv::Vec3d(20.475, 0.049, -0.972));
 
 /* Macbeth ColorChecker with 2deg D50 */
 const Color Macbeth_D50_2(ColorChecker2005_LAB_D50_2, Lab_D50_2);
